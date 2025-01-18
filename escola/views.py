d73ae9405767c872a9dd22c1ba8cdc07c429a86b
Thomaz Halter <thomaz.halter@outlook.com>
2025-01-18 11:39:58 -0300

    end project

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
-from escola.models import Estudante, Curso
-from escola.serializers import EstudanteSerializer, CursoSerializer
-from rest_framework import viewsets
+from escola.models import Estudante, Curso, Matricula
+from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasCursoSerializer, ListaMatriculasEstudanteSerializer
+from rest_framework import viewsets, generics
 
 class EstudanteViewSet(viewsets.ModelViewSet):
     queryset = Estudante.objects.all()
@@ -10,4 +10,18 @@ class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
 
+class MatriculaViewSet(viewsets.ModelViewSet):
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
 
+class ListaMatriculaEstudante(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(estudante_id = self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasEstudanteSerializer
+
+class ListaMatriculaCurso(generics.ListAPIView):
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id = self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasCursoSerializer
--- setup/urls.py
+++ setup/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import EstudanteViewSet, CursoViewSet
+from escola.views import EstudanteViewSet, CursoViewSet, MatriculaViewSet, ListaMatriculaEstudante, ListaMatriculaCurso
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('estudantes', EstudanteViewSet, 'Estudantes')
 router.register('cursos', CursoViewSet, 'Cursos')
+router.register('matricula', MatriculaViewSet, 'Matricula')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('estudantes/<int:pk>/matriculas', ListaMatriculaEstudante.as_view()),
+    path('cursos/<int:pk>/matriculas', ListaMatriculaCurso.as_view()),
 ]
